d7195f23b76afe939d5469d0b649e1faa43e0b02
Guy Symonds <guy@redspartan.co.uk>
2023-01-14 11:33:38 +0000

    updated files before re-creating them all

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('root')
+);
